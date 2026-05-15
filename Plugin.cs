0b9c6f69f62794a42be7e948707cb2c22ebe9170
InvoluntaryChud <stockdale.andrewj@gmail.com>
2026-05-15 19:28:15 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -2,18 +2,32 @@
 using BepInEx.Configuration;
 using BepInEx.Logging;
 using tarkin.doordash.Patches;
+using Fika.Core.Networking;
+using Fika.Core.Main.Utils;
+using Fika.Core.Main.Players;
+using Fika.Core.Modding;
+using Fika.Core.Modding.Events;
+using Fika.Core.Main.Components;
+using Fika.Core.Networking.LiteNetLib;
+using Fika.Core.Networking.LiteNetLib.Utils;
+using EFT;
+using EFT.Interactive;
+using Comfort.Common;
+using UnityEngine;
 
 namespace tarkin.doordash
 {
-    [BepInPlugin("com.tarkin.doordash", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+
+    public enum GameObjectType
+    {
+        Door = 1,
+    }
+    [BepInPlugin("com.tarkin.doordash", "DoorDash-Fika", "1.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        internal static new ManualLogSource Log;
-
+        internal static ManualLogSource Log;
+        private readonly NetPacketProcessor packetProcessor = new();
         internal static ConfigEntry<bool> Enabled;
-
-        internal static ConfigEntry<bool> BreachLocked;
-
         internal static ConfigEntry<float> VelocityThresholdSqr;
         internal static ConfigEntry<float> RayDistance;
 
@@ -21,8 +35,6 @@ namespace tarkin.doordash
         internal static ConfigEntry<float> DislodgeForce;
 
         internal static ConfigEntry<float> ArmDamageBase;
-        internal static ConfigEntry<float> LockedBreachDamageMultiplier;
-        internal static ConfigEntry<float> ContusionTime;
         internal static ConfigEntry<float> ContusionStrength;
         internal static ConfigEntry<float> RecoilHands;
         internal static ConfigEntry<float> RecoilCamera;
@@ -31,24 +43,67 @@ namespace tarkin.doordash
 
         private void Awake()
         {
-            Log = base.Logger;
+            Log = Logger;
 
             InitConfiguration();
 
             new Patch_Door_KickOpen().Enable();
 
-            new Patch_GameWorld_OnGameStarted().Enable();
-            Patch_GameWorld_OnGameStarted.OnPostfix += (gameWorld) =>
+            FikaEventDispatcher.SubscribeEvent<GameWorldStartedEvent>(OnGameWorldStarted);
+            FikaEventDispatcher.SubscribeEvent<FikaNetworkManagerCreatedEvent>(OnFikaNetworkManagerCreated);
+
+            packetProcessor.SubscribeNetSerializable<SyncOpenStatePacket, NetPeer>(OnSyncOpenStatePacketReceived);
+        }
+
+        private void OnGameWorldStarted(GameWorldStartedEvent obj)
+        {
+            if (Application.isBatchMode)
+                return;
+            
+            obj.GameWorld.MainPlayer.gameObject.GetOrAddComponent<RaycastBreacher>();
+        }
+
+        void OnFikaNetworkManagerCreated (FikaNetworkManagerCreatedEvent ev)
+        {
+            switch (ev.Manager)
             {
-                gameWorld.MainPlayer.gameObject.GetOrAddComponent<RaycastBreacher>();
-            };
+                case FikaServer server:
+                    server.RegisterPacket<SyncOpenStatePacket, NetPeer>(OnSyncOpenStatePacketReceived);                    
+                break;
+                case FikaClient client:
+                    client.RegisterPacket<SyncOpenStatePacket, NetPeer>(OnSyncOpenStatePacketReceived);
+                break;
+            }
+        }
+
+        private void OnSyncOpenStatePacketReceived(SyncOpenStatePacket packet, NetPeer peer)
+        {
+            if (!CoopHandler.TryGetCoopHandler(out CoopHandler coopHandler) ||
+                !coopHandler.Players.TryGetValue(packet.netID, out _)) 
+                return;
+
+            WorldInteractiveObject worldInteractiveObject = Singleton<GameWorld>.Instance.FindDoor(packet.objectID);
+
+            if (worldInteractiveObject is null || 
+                !worldInteractiveObject.isActiveAndEnabled) 
+                return;
+            
+            Door door = (Door)worldInteractiveObject;
+
+            if (door.DoorState != EDoorState.Open)
+            {
+                door.DoorState = EDoorState.Shut;
+                door.KickOpen(true);
+                coopHandler.MyPlayer.UpdateInteractionCast();
+            }
+
+            if (FikaBackendUtils.IsServer)
+                Singleton<FikaServer>.Instance.SendData(ref packet, DeliveryMethod.ReliableOrdered, true);
         }
 
         private void InitConfiguration()
         {
             Enabled = Config.Bind("", "Enabled", true);
-
-            BreachLocked = Config.Bind("Logic", "Breach Locked", false, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
 
             VelocityThresholdSqr = Config.Bind("Sprint Ram", "Velocity Threshold", 20f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             RayDistance = Config.Bind("Sprint Ram", "Ray Distance", 0.7f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
@@ -56,9 +111,7 @@ namespace tarkin.doordash
             DislodgeChance = Config.Bind("Physical Door", "Chance To Dislodge On Breach", 0.01f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             DislodgeForce = Config.Bind("Physical Door", "Dislodge Force", 10f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
 
-            ArmDamageBase = Config.Bind("Player Effect", "Health Damage", 10f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
-            LockedBreachDamageMultiplier = Config.Bind("Player Effect", "LockedBreachDamageMultiplier", 2f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
-            ContusionTime = Config.Bind("Player Effect", "Contusion Time", 0.5f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
+            ArmDamageBase = Config.Bind("Player Effect", "Arm Damage", 10f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             ContusionStrength = Config.Bind("Player Effect", "Contusion Strength", 0.5f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             RecoilHands = Config.Bind("Player Effect", "Recoil Hands", 2f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
             RecoilCamera = Config.Bind("Player Effect", "Recoil Camera", 4f, new ConfigDescription("", null, new ConfigurationManagerAttributes { IsAdvanced = true }));
